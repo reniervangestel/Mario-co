671c1ec0efd7512047fb22409ecd8b908d59bcc6
Renier <renier@me.com>
2015-01-08 14:42:10 +0100

    V0.2
    MainActivity heeft spinner en laat informatie zien over de
    geselecteerde optie.

--- app/src/main/java/com/example/renier/marioco/MainActivity.java
+++ app/src/main/java/com/example/renier/marioco/MainActivity.java
@@ -4,14 +4,49 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import java.util.ArrayList;
+import java.util.List;
 
 
-publicommit class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnItemSelectedListener {
+
+
+    Spinner spinner;
+    ListView service;
+    ArrayAdapter<String> adapter;
+    List<String> list;
+    TextView serviceinfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = new ArrayList<String>();
+        list.add("Riolering");
+        list.add("Dak Lekkage");
+        list.add("Prinses In Nood");
+
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        this.spinner = spinner;
+
+        TextView serviceinfo = (TextView)findViewById(R.id.Veld1);
+        this.serviceinfo = serviceinfo;
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+
     }
 
 
@@ -20,6 +55,39 @@ publicommit class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position,
+                               long id) {
+
+        switch(position){
+            case 0: //Riolering
+                System.out.println("Riolering");
+
+                serviceinfo.setText("Uw toiletproblemen in mum van tijd verholpen!");
+
+                break;
+            case 1: //Dak Lekkages
+                System.out.println("Dak Lekkages");
+
+                serviceinfo.setText("Valt alles in het water? Wij helpen u uit de brand!");
+
+                break;
+            case 2: //Prinses In Nood
+                System.out.println("Prinses In Nood");
+
+                serviceinfo.setText("Wij vinden het juiste kasteel voor u!");
+                break;
+        }
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+
     }
 
     @Override
